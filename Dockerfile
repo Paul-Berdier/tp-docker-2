9ce160cc133d976135e9040edc30955276608479
Paul-Berdier <paul.berdo@hotmail.com>
2024-07-14 22:06:47 +0000

    Add Dockerfile, complete docker-compose.yml, and update src/index.js for MySQL connection

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM node:14
-
 WORKDIR /usr/src/app
 
 COPY package*.json ./
 
 RUN npm install --only=production
 
+# Installer netcat
+RUN apt-get update && apt-get install -y netcat
+
 COPY . .
 
 EXPOSE 3000
 
-CMD ["node", "src/app.js"]
+CMD ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
-'use strict';
-
+const mysql = require('mysql');
 const express = require('express');
-var mysql = require('mysql');
-
-const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0';
-
 const app = express();
-app.get('/', (req, res) => {
-  var connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST || '0.0.0.0' ,
-    port: process.env.DATABASE_PORT || 3306,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-  })
 
-  connection.connect(function (error) {
-    if (error) {
-      console.log("Connection to MySQL failed.");
-      throw error
-    }
-  });
-
-  const query = 'SELECT SUM(1+1) as sum'
-  connection.query(query, function (error, result) {
-    if (error) throw error;
-    res.send(`'${query}' => ${result[0].sum}`);
-  });
-
-  connection.end
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on port ${PORT}`);
+connection.connect((err) => {
+  if (err) {
+    console.error('Connection to MySQL failed:', err);
+    process.exit(1);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) throw error;
+    res.send(`The solution is: ${results[0].solution}`);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Running on port 3000');
+});
+
